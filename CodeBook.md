d3696d7e0ebed9af8f112e1f68318e33851f08ff
kev716 <kev716@gmail.com>
2017-08-04 11:19:32 -0700

    Add files via upload

--- CodeBook.md
+++ CodeBook.md
@@ -1,12 +1,12 @@
 # Introduction
 
-This project uses text files from the Human Activity Recognition Using Smartphones Data Set project from the UCI Machine Learning Repository (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones). The text files are combined into a tidy dataset, and then means of a subset of variables are calculated.
+This project uses text files from the Human Activity Recognition Using Smartphones Data Set project from the UCI Machine Learning Repository (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones). The text files are combined into a tidy dataset, and then grouped means of a subset of variables are calculated.
 
 The dataset consists of  a series of measurements taken from smartphone sensors as the 30 subjects participated in 6 different activities such as laying, walking, and climbing stairs. 
 
 # Files and Variables
 
-There were The data have been divided into training and test datasets with identical structures. These were combined for this project.
+The data have been divided into training and test datasets with identical structures. These were combined for this project.
 
 Each dataset contained the following files:
 
@@ -21,3 +21,14 @@ In addition, the following files describing the data were included:
 2. activity_labels.txt - this is a 2 column table. Column 1 contains an integer index that links to the integer in the y-test.txt file. Column 2 is a character description of the activity.
 
 In addition, README.txt and features_info.txt files described the experiment and the variables included in the dataset.
+
+# Dataset Tidying and Final Computation
+The scripts in run_analysis.R contain the functions that were created for this project. The basic steps are:
+1. Read the data from each of the above described text files
+2. Combine the test and training tables for subjects, activities, and measures
+3. Add variable names and labels
+4. Extract only measures of means and standard deviations
+5. Merge the 3 tables
+6. Group the data by subjects and activity
+7. Compute grouped means for each measure
+8. Export the final table as a text file
--- run_analysis.R
+++ run_analysis.R
@@ -6,81 +6,92 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average 
 #   of each variable for each activity and each subject.
 
-Wk4Assign <- function() {
-    library(dplyr)
-    library(readr)
-    library(curl)
-    
-  # Check if directory exists and downloads it if it doesn't
-    if(!file.exists("HAR.zip")) {
-      url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-      download.file(url,
-        destfile='HAR.zip',
-        method="curl", # for OSX / Linux 
-        mode="wb") # "wb" means "write binary," and is used for binary files
-      unzip(zipfile = "HAR.zip") # unpack the files into subdirectories 
-    }
-
-  # Read test & train subject files
-    subjects_test <- read_fwf("UCI HAR Dataset/test/subject_test.txt", 
-                             fwf_empty("UCI HAR Dataset/test/subject_test.txt"))
-    subjects_train <- read_fwf("UCI HAR Dataset/train/subject_train.txt", 
-                              fwf_empty("UCI HAR Dataset/train/subject_train.txt"))
-    
-    # Combine subject files and name its only column
-    subjects <- rbind(subjects_test, subjects_train)
-    names(subjects) <- "subject"
-
-    # Read y test & train files, which contain activities performed
-    activities_test <- read_fwf("UCI HAR Dataset/test/y_test.txt", 
-                             fwf_empty("UCI HAR Dataset/test/y_test.txt"))
-    activities_train <- read_fwf("UCI HAR Dataset/train/y_train.txt", 
-                       fwf_empty("UCI HAR Dataset/train/y_train.txt"))
-    
-    # Combine 2 y files and name its only column activity
-    activities <- rbind(activities_test, activities_train)
-    names(activities) <- "activity"
-    
-    # Read activity labels
-    actLabels <- read.table("UCI HAR Dataset/activity_labels.txt", sep = " ")
-    
-    # First column of actLabels contains numeric code that matches activities column
-    # second column contains description of activity
-    actNum <- as.vector(actLabels$V1)
-    names(actNum) <- as.vector(actLabels$V2)
-    
-    # Match actNum variables with activities & substitute descriptions
-    activities$activity <- names(actNum)[match(activities$activity, actNum)]
+tidyHarData <- function() {
   
-    # Read X test & train files, which contain sensor measures, and combine
-    measures_test <- read_fwf("UCI HAR Dataset/test/X_test.txt", 
-                       fwf_empty("UCI HAR Dataset/test/X_test.txt"))
-    measures_train <- read_fwf("UCI HAR Dataset/train/X_train.txt", 
-                        fwf_empty("UCI HAR Dataset/train/X_train.txt"))
-    
-    measures <- rbind(measures_test, measures_train)
-    
-    # read features file which has measures variables names, and use 2nd column to name 
-    #  measure variables
-    measureLabels <- read.table("UCI HAR Dataset/features.txt", sep = " ")
-    names(measures) <- as.vector(measureLabels$V2)
-    
-    # Extract columns from measures that contin mean or standard deviation
-    measuresSel <- measures[, grep(paste(c("mean", "std"), collapse = "|"), colnames(measures))]
-    
-    # Combine subjects, activities & measuresSel data tables
-    HarTidyData <- do.call(cbind, list(subjects, activities, measuresSel))   
-    
-    # Remove temporary objects
-    rm(activities, activities_test, activities_train, actLabels, measureLabels, measures, 
-       measures_test, measures_train, measuresSel, subjects, subjects_test, subjects_train, 
-       actNum)
+  library(dplyr)
+  library(curl)
+  library(readr)
+  
+  # Check if directory exists, download it if it doesn't
+  if(!file.exists("HAR.zip")) {
+    url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(url,
+                  destfile='HAR.zip',
+                  method="curl", # for OSX / Linux 
+                  mode="wb") # write binary
+    unzip(zipfile = "HAR.zip") # unpack the files into subdirectories 
+  }
+  
+  # Read, combine and modify variables for subjects, activities and summary variables
+  subjects <- getSubjects()
+  activities <- getActivities()
+  measures <- getMeasures()
+  
+  # Combine subjects, activities & measures data tables
+  HarTidyData <- do.call(cbind, list(subjects, activities, measures))   
+  
+  # Create new table by grouping HarTidyData by subjects & activities, then find
+  #  means for all ungrouped columns
+  dataGrouped <- group_by(HarTidyData, subject, activity)
+  HarMeans <- summarise_all(dataGrouped, mean)
+}
 
-    # Create 2nd table by grouping HarTidyData by subjects & activities, then find
-    #  means for all ungrouped columns
-    dataGrouped <- group_by(HarTidyData, subject, activity)
-    HarMeans <- summarise_all(dataGrouped, mean)
-    
-    #Remove temporary table
-    rm(dataGrouped)
+getSubjects <- function() {
+  
+  # Read test & train subject files
+  subjects_test <- read_fwf("UCI HAR Dataset/test/subject_test.txt", 
+                            fwf_empty("UCI HAR Dataset/test/subject_test.txt"))
+  subjects_train <- read_fwf("UCI HAR Dataset/train/subject_train.txt", 
+                             fwf_empty("UCI HAR Dataset/train/subject_train.txt"))
+  
+  # Combine files and name its only column
+  subjects <- rbind(subjects_test, subjects_train)
+  names(subjects) <- "subject"
+  return(subjects)
+}
+
+getActivities <- function() {
+  
+  # Read y test & train files, which contain activities performed
+  activities_test <- read_fwf("UCI HAR Dataset/test/y_test.txt", 
+                              fwf_empty("UCI HAR Dataset/test/y_test.txt"))
+  activities_train <- read_fwf("UCI HAR Dataset/train/y_train.txt", 
+                               fwf_empty("UCI HAR Dataset/train/y_train.txt"))
+  
+  # Combine 2 y files and name its only column
+  activities <- rbind(activities_test, activities_train)
+  names(activities) <- "activity"
+  
+  # Read activity labels
+  actLabels <- read.table("UCI HAR Dataset/activity_labels.txt", sep = " ")
+  
+  # First column of actLabels contains numeric code that matches activities column
+  # second column contains description of activity
+  actNum <- as.vector(actLabels$V1)
+  names(actNum) <- as.vector(actLabels$V2)
+  
+  # Match actNum variables with activities & substitute descriptions
+  activities$activity <- names(actNum)[match(activities$activity, actNum)]
+  return(activities)
+}
+
+getMeasures <- function() {
+  
+  # Read X test & train files, which contain sensor measures, and combine
+  
+  measures_test <- read_fwf("UCI HAR Dataset/test/X_test.txt", 
+                            fwf_empty("UCI HAR Dataset/test/X_test.txt"))
+  measures_train <- read_fwf("UCI HAR Dataset/train/X_train.txt", 
+                             fwf_empty("UCI HAR Dataset/train/X_train.txt"))
+  
+  measures <- rbind(measures_test, measures_train)
+  
+  # read features file which has measures variables names, and use 2nd column to name 
+  #  measure variables
+  measureLabels <- read.table("UCI HAR Dataset/features.txt", sep = " ")
+  names(measures) <- as.vector(measureLabels$V2)
+  
+  # Extract columns from measures that contin mean or standard deviation
+  measures <- measures[, grep(paste(c("mean", "std"), collapse = "|"), colnames(measures))]
+  return(measures)
 }
